865dd28ddd6a361abbdc64d686c171ea0d5eeb8f
Jnarrr <lfebruary60@gmail.com>
2022-12-16 22:33:48 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+//import { NavigationContainer, TabActions } from '@react-navigation/native';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const aquaApp = () => {
+  return (
+    <Text>Test</Text>
+  );
+};
+
+export default aquaApp;
